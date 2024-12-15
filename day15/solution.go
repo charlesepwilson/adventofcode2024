d170392965a5c870b07a6d35df94efe7a2fd81be
Charlie Wilson <charles.ep.wilson@gmail.com>
2024-12-15 20:37:34 +0000

    d15p2 complete

--- day15/solution.go
+++ day15/solution.go
@@ -15,19 +15,19 @@ func (Solution) Part1(input []byte) int {
 	for _, instruction := range instructions {
 		robotPos, grid = followInstruction(robotPos, grid, instruction)
 	}
-	result := 0
-	for i, line := range grid {
-		for j, item := range line {
-			if item == BOX {
-				result += 100*i + j
-			}
-		}
-	}
-	return result
+	return computeTotalGps(grid, BOX)
 }
 
 func (Solution) Part2(input []byte) int {
-	return len(input)
+	grid, instructions, robotPos := parseInput(input)
+	grid = widenGrid(grid)
+	robotPos.Right = 2 * robotPos.Right
+	for _, instruction := range instructions {
+		//fmt.Println("robot", robotPos, "dir", instruction)
+		//fmt.Println(&grid)
+		robotPos, grid = followWideInstruction(robotPos, grid, instruction)
+	}
+	return computeTotalGps(grid, LEFTBOX)
 }
 
 func (Solution) GetExample(part int) []byte {
@@ -38,13 +38,15 @@ func (Solution) ExampleAnswer1() int {
 	return 10092
 }
 func (Solution) ExampleAnswer2() int {
-	return 0
+	return 9021
 }
 
 const ROBOT = '@'
 const BOX = 'O'
 const WALL = '#'
 const OUTSIDE = ';'
+const LEFTBOX = '['
+const RIGHTBOX = ']'
 
 func parseInput(input []byte) (grid Grid, instructions []utils.VectorI, start utils.VectorI) {
 	gridPart, instructionsPart, _ := bytes.Cut(input, []byte("\n\n"))
@@ -130,4 +132,104 @@ func followInstruction(robotPos utils.VectorI, grid Grid, instruction utils.Vect
 		grid.Set(newRobotPos, ROBOT)
 		return newRobotPos, grid
 	}
+}
+
+func widenGrid(grid Grid) Grid {
+	newGrid := make(Grid, len(grid))
+	for i, row := range grid {
+		newRow := make([]byte, 0, len(row)*2)
+		for _, char := range row {
+			var n []byte
+			if char == BOX {
+				n = []byte{LEFTBOX, RIGHTBOX}
+			} else {
+				n = []byte{char, char}
+			}
+			newRow = append(newRow, n...)
+		}
+		newGrid[i] = newRow
+	}
+	return newGrid
+}
+
+func followWideInstruction(robotPos utils.VectorI, grid Grid, instruction utils.VectorI) (newRobotPos utils.VectorI, newGrid Grid) {
+	newRobotPos = robotPos.Add(instruction)
+	itemAtNewRobotPos := grid.Get(newRobotPos)
+	if itemAtNewRobotPos == WALL {
+		return robotPos, grid
+	} else if itemAtNewRobotPos == LEFTBOX || itemAtNewRobotPos == RIGHTBOX {
+		success := canPush(newRobotPos, grid, instruction)
+		if !success {
+			return robotPos, grid
+		} else {
+			applyPush(newRobotPos, grid, instruction)
+			grid.Set(newRobotPos, ROBOT)
+			return newRobotPos, grid
+		}
+	} else {
+		grid.Set(newRobotPos, ROBOT)
+		return newRobotPos, grid
+	}
+}
+
+func canPush(pushObjAt utils.VectorI, grid Grid, instruction utils.VectorI) bool {
+	obj := grid.Get(pushObjAt)
+	if obj == WALL {
+		return false
+	} else if obj == LEFTBOX || obj == RIGHTBOX {
+		pushInto := pushObjAt.Add(instruction)
+		if instruction.Right == 0 {
+			// vertical; need to handle wide boxes
+			var offset int
+			if obj == LEFTBOX {
+				offset = 1
+			} else {
+				offset = -1
+			}
+			otherBoxPart := pushObjAt.Add(utils.VectorI{Right: offset})
+			otherPushInto := otherBoxPart.Add(instruction)
+			success := canPush(pushInto, grid, instruction) && canPush(otherPushInto, grid, instruction)
+			return success
+		} else {
+			return canPush(pushInto, grid, instruction)
+		}
+	} else {
+		return true
+	}
+}
+
+func applyPush(pushObjAt utils.VectorI, grid Grid, instruction utils.VectorI) {
+	obj := grid.Get(pushObjAt)
+	if obj == LEFTBOX || obj == RIGHTBOX {
+		pushInto := pushObjAt.Add(instruction)
+		applyPush(pushInto, grid, instruction)
+		grid.Set(pushInto, obj)
+		grid.Set(pushObjAt, ROBOT)
+		if instruction.Right == 0 {
+			// vertical; need to handle wide boxes
+			var offset int
+			if obj == LEFTBOX {
+				offset = 1
+			} else {
+				offset = -1
+			}
+			otherBoxPart := pushObjAt.Add(utils.VectorI{Right: offset})
+			otherPushInto := otherBoxPart.Add(instruction)
+			applyPush(otherPushInto, grid, instruction)
+			grid.Set(otherPushInto, grid.Get(otherBoxPart))
+			grid.Set(otherBoxPart, '.')
+		}
+	}
+}
+
+func computeTotalGps(grid Grid, target byte) int {
+	result := 0
+	for i, line := range grid {
+		for j, item := range line {
+			if item == target {
+				result += 100*i + j
+			}
+		}
+	}
+	return result
 }
